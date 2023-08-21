c2d1cfc5d8e0ad23611fe2a88eda24221448e617
BradDesktop <brad.martin@xdisplay.com>
2023-08-21 08:15:33 -0400

    cleanup

--- buildWASM.py
+++ buildWASM.py
@@ -9,15 +9,14 @@ argParser = argparse.ArgumentParser(
     epilog='Requires setup of Raylib (youtu.be/-F6THkPkF2I) and emcc (youtu.be/j6akryezlzc)')
 args = vars(argParser.parse_args())
 
-inputPath = "MountainClimber\MountainClimber.cpp"
+inputPath = os.path.join(cwd, 'main.cpp')
 if not os.path.exists(inputPath):
     print('Input path {} does not exist'.format(inputPath))
     exit(0)
-outputPath = os.path.join(cwd, 'game.html')
+outputPath = os.path.join(cwd, 'build', 'game.html')
 
 # Build WASM
 raylibSrcDir = 'C:/raylib/raylib/src'
-compilerArgs = "-Wall -std=c++14 -D_DEFAULT_SOURCE -Wno-missing-braces -Wunused-result -Os -I. -I {0} -I {0}/external -L. -L {0} -s USE_GLFW=3 -s ASYNCIFY -s TOTAL_MEMORY=67108864 -s FORCE_FILESYSTEM=1 --shell-file ./shell.html {0}/web/libraylib.a -DPLATFORM_WEB -s EXPORTED_FUNCTIONS=[\"_free\",\"_malloc\",\"_main\"] -s EXPORTED_RUNTIME_METHODS=ccall".format(
-    raylibSrcDir)
+compilerArgs = "-Wall -std=c++14 -D_DEFAULT_SOURCE -Wno-missing-braces -Wunused-result -Os -I. -I {0} -I {0}/external -L. -L {0} -s USE_GLFW=3 -s ASYNCIFY -s TOTAL_MEMORY=67108864 -s FORCE_FILESYSTEM=1 --shell-file ./build/shell.html {0}/web/libraylib.a -DPLATFORM_WEB -s EXPORTED_FUNCTIONS=[\"_free\",\"_malloc\",\"_main\"] -s EXPORTED_RUNTIME_METHODS=ccall".format(raylibSrcDir)
 build_cmd = "emcc -o {} {} {}".format(outputPath, inputPath, compilerArgs)
 os.system(build_cmd)
